6e69484c96ab9c412844852c79079dcf95311bdc
Annique Nsabimana <anniquensabimana@gmail.com>
2019-07-18 18:49:30 -0500

    createdStyling

--- src/App.js
+++ src/App.js
@@ -7,45 +7,41 @@ import "./App.css";
 import ApodTitle from "./Components/ApodTitle"
 import ApodMediaImage from "./Components/ApodMediaImage";
 import DateSelector from "./Components/DateSelector";
-
+import {Container} from "./Components/StyleElement";
 function App() {
   const [data, setData] = useState();
-  const [date, setDate] = useState("2019-07-17");
+  const [date, setDate] = useState("");
 
   useEffect(() => {
-    const getData = async () => {
-      return await axios.get("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo", {
-        params: {
-          date: date,
-          api_key: "cvQfsM1Yd2opQ3UAQEufPMMPRinnTReNGgUeirlt"
-        }
-      });
-    };
-
-    getData()
-      .then(res => {
-        setData(res.data);
+    
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo")
+     .then(result => {
+       const res = result.data
+       console.log (result.data)
+        setDate(res.date);
       })
       .catch(err => {
         console.log(err);
-      });
-  }, [date]);
+  
+  }, []);
 
-  const handleChange = event => {
-    setDate(event.target.value);
-  };
+  // const handleChange = event => {
+  //   setDate(event.target.value);
+  // };
   //Message while the data is fetching
-  if (!data) {
-    return <div className="loading">Loading...</div>;
-  }
+  // if (!data) {
+  //   return <div className="loading">Loading...</div>;
+  // }
 
-  return (
-    <div className="App">
-      <ApodTitle />
-      <DateSelector handleChange={handleChange} />
-      <ApodMediaImage data={data} />
-    </div>
-  );
+  
+});
+return (
+  <Container>
+  {/* <ApodTitle />
+  <DateSelector/>
+  // <ApodMediaImage data={data} /> */}
+  {date}
+  </Container>
+);
 }
-
 export default App;
